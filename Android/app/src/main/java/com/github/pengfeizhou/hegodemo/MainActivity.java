36c5029935a67edf678bf0f7de6d9bbb03d20b52
pengfei.zhou <pengfeizhou@foxmail.com>
2019-07-18 13:29:28 +0800

    init android project

---
+++ Android/app/src/main/java/com/github/pengfeizhou/hegodemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.pengfeizhou.hegodemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.pengfeizhou.hegodemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

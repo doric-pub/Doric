f86e7623a2e95588a93a62cf2346d654b176f745
pengfei.zhou <pengfeizhou@foxmail.com>
2019-07-25 19:26:33 +0800

    init iOS module

---
+++ iOS/Example/Pods/Target Support Files/Doric/Doric-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Doric : NSObject
+@end
+@implementation PodsDummy_Doric
+@end
---
+++ iOS/Example/Pods/Target Support Files/Pods-Doric_Example/Pods-Doric_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Doric_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Doric_Example
+@end

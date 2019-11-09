053166ab7b71aada541db90f18789228cada11c0
王劲鹏 <jingpeng.wang@outlook.com>
2019-11-09 16:57:45 +0800

    add qrcode scan on dev kit connect

--- Android/doric/src/main/java/pub/doric/dev/DevPanel.java
+++ Android/doric/src/main/java/pub/doric/dev/DevPanel.java
@@ -1,5 +1,7 @@
 package pub.doric.dev;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +11,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 import pub.doric.R;
 
 public class DevPanel extends BottomSheetDialogFragment {
@@ -26,5 +31,29 @@ public class DevPanel extends BottomSheetDialogFragment {
             @Nullable Bundle savedInstanceState
     ) {
         return inflater.inflate(R.layout.layout_dev, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        getView().findViewById(R.id.menu1_text_view).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final RxPermissions rxPermissions = new RxPermissions(DevPanel.this);
+                Disposable disposable = rxPermissions
+                        .request(Manifest.permission.CAMERA)
+                        .subscribe(new Consumer<Boolean>() {
+                            @Override
+                            public void accept(Boolean grant) throws Exception {
+                                if (grant){
+                                    Intent intent = new Intent(getContext(), ScanQRCodeActivity.class);
+                                    getContext().startActivity(intent);
+                                }
+                            }
+                        });
+
+            }
+        });
     }
 }
---
+++ Android/doric/src/main/java/pub/doric/dev/ScanQRCodeActivity.java
@@ -0,0 +1,72 @@
+package pub.doric.dev;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import cn.bingoogolapple.qrcode.core.QRCodeView;
+import cn.bingoogolapple.qrcode.zxing.ZXingView;
+import pub.doric.R;
+
+public class ScanQRCodeActivity extends AppCompatActivity implements QRCodeView.Delegate {
+
+    private ZXingView mZXingView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.layout_scan_qrcode);
+        mZXingView = findViewById(R.id.zxingview);
+        mZXingView.setDelegate(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mZXingView.startCamera();
+        mZXingView.startSpotAndShowRect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        mZXingView.stopCamera();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mZXingView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onScanQRCodeSuccess(String result) {
+        setTitle("扫描结果为：" + result);
+    }
+
+    @Override
+    public void onCameraAmbientBrightnessChanged(boolean isDark) {
+        String tipText = mZXingView.getScanBoxView().getTipText();
+        String ambientBrightnessTip = "\n环境过暗，请打开闪光灯";
+        if (isDark) {
+            if (!tipText.contains(ambientBrightnessTip)) {
+                mZXingView.getScanBoxView().setTipText(tipText + ambientBrightnessTip);
+            }
+        } else {
+            if (tipText.contains(ambientBrightnessTip)) {
+                tipText = tipText.substring(0, tipText.indexOf(ambientBrightnessTip));
+                mZXingView.getScanBoxView().setTipText(tipText);
+            }
+        }
+    }
+
+    @Override
+    public void onScanQRCodeOpenCameraError() {
+        System.out.println();
+    }
+}

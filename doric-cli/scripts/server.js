40bcdd689818f3bb96e289a3a1de3f9c7b16bd3e
王劲鹏 <jingpeng.wang@outlook.com>
2019-11-12 16:43:30 +0800

    launch vscode via child process

--- doric-cli/scripts/server.js
+++ doric-cli/scripts/server.js
@@ -1,4 +1,5 @@
 const ws = require('nodejs-websocket')
+const { spawn } = require('child_process')
 
 const createServer = () => {
     let server = ws.createServer(connection => {
@@ -11,6 +12,19 @@ const createServer = () => {
                     let contextId = resultObject.data.contextId
                     let projectHome = resultObject.data.projectHome
                     console.log(projectHome)
+
+                    const code = spawn('code', [projectHome])
+                    code.stdout.on('data', (data) => {
+                        console.log(`stdout: ${data}`)
+                    })
+                    
+                    code.stderr.on('data', (data) => {
+                        console.error(`stderr: ${data}`)
+                    })
+                      
+                    code.on('close', (code) => {
+                        console.log(`child process exited with code ${code}`)
+                    });
                     break
             }
         })

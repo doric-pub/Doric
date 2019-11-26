0b1174c3fd4bd8315467da307e9d0ecb7ef11987
pengfei.zhou <pengfeizhou@foxmail.com>
2019-11-26 14:06:44 +0800

    feat:Refreshable +list

--- demo/src/ListDemo.ts
+++ demo/src/ListDemo.ts
@@ -1,15 +1,11 @@
-import { Group, Panel, List, text, gravity, Color, Stack, LayoutSpec, list, NativeCall, listItem, log, vlayout, Gravity, hlayout, Text } from "doric";
-const colors = [
-    "#f0932b",
-    "#eb4d4b",
-    "#6ab04c",
-    "#e056fd",
-    "#686de0",
-    "#30336b",
-]
+import { Group, Panel, List, text, gravity, Color, Stack, LayoutSpec, list, NativeCall, listItem, log, vlayout, Gravity, hlayout, Text, refreshable, Refreshable, ListItem } from "doric";
+import { rotatedArrow, colors } from "./utils";
+import { isObject } from "util";
 @Entry
 class ListPanel extends Panel {
     build(rootView: Group): void {
+        let refreshView: Refreshable
+        let offset = Math.ceil(Math.random() * colors.length)
         vlayout([
             text({
                 text: "ListDemo",
@@ -23,76 +19,90 @@ class ListPanel extends Panel {
                 textAlignment: gravity().center(),
                 height: 50,
             }),
-            list({
-                itemCount: 1000,
-                renderItem: (idx: number) => {
-                    let counter!: Text
-                    return listItem(
-                        hlayout([
-                            text({
-                                layoutConfig: {
-                                    widthSpec: LayoutSpec.WRAP_CONTENT,
-                                    heightSpec: LayoutSpec.EXACTLY,
-                                    alignment: gravity().center(),
-                                },
-                                text: `Cell At Line ${idx}`,
-                                textAlignment: gravity().center(),
-                                textColor: Color.parse("#ffffff"),
-                                textSize: 20,
-                                height: 50,
-                            }),
-                            text({
-                                textColor: Color.parse("#ffffff"),
-                                textSize: 20,
-                                text: "",
-                            }).also(it => {
-                                counter = it
-                                it.layoutConfig = {
-                                    widthSpec: LayoutSpec.WRAP_CONTENT,
-                                    heightSpec: LayoutSpec.WRAP_CONTENT,
-                                    margin: {
-                                        left: 10,
+            refreshView = refreshable({
+                onRefresh: () => {
+                    refreshView.setRefreshing(context, false).then(() => {
+                        (refreshView.content as List).also(it => {
+                            it.reset()
+                            offset = Math.ceil(Math.random() * colors.length)
+                            it.itemCount = 40
+                            it.renderItem = (idx: number) => {
+                                let counter!: Text
+                                return listItem(
+                                    hlayout([
+                                        text({
+                                            layoutConfig: {
+                                                widthSpec: LayoutSpec.WRAP_CONTENT,
+                                                heightSpec: LayoutSpec.EXACTLY,
+                                                alignment: gravity().center(),
+                                            },
+                                            text: `Cell At Line ${idx}`,
+                                            textAlignment: gravity().center(),
+                                            textColor: Color.parse("#ffffff"),
+                                            textSize: 20,
+                                            height: 50,
+                                        }),
+                                        text({
+                                            textColor: Color.parse("#ffffff"),
+                                            textSize: 20,
+                                            text: "",
+                                        }).also(it => {
+                                            counter = it
+                                            it.layoutConfig = {
+                                                widthSpec: LayoutSpec.WRAP_CONTENT,
+                                                heightSpec: LayoutSpec.WRAP_CONTENT,
+                                                margin: {
+                                                    left: 10,
+                                                }
+                                            }
+                                        })
+                                    ]).also(it => {
+                                        it.layoutConfig = {
+                                            widthSpec: LayoutSpec.AT_MOST,
+                                            heightSpec: LayoutSpec.WRAP_CONTENT,
+                                            margin: {
+                                                bottom: 2,
+                                            }
+                                        }
+                                        it.gravity = gravity().center()
+                                        it.bgColor = colors[(idx + offset) % colors.length]
+                                        let clicked = 0
+                                        it.onClick = () => {
+                                            counter.text = `Item Clicked ${++clicked}`
+                                        }
+                                    })
+                                ).also(it => {
+                                    it.layoutConfig = {
+                                        widthSpec: LayoutSpec.AT_MOST,
+                                        heightSpec: LayoutSpec.WRAP_CONTENT,
                                     }
-                                }
-                            })
-                        ]).also(it => {
-                            it.layoutConfig = {
-                                widthSpec: LayoutSpec.AT_MOST,
-                                heightSpec: LayoutSpec.WRAP_CONTENT,
-                                margin: {
-                                    bottom: 2,
-                                }
-                            }
-                            it.gravity = gravity().center()
-                            it.bgColor = Color.parse(colors[idx % colors.length])
-                            let clicked = 0
-                            it.onClick = () => {
-                                counter.text = `Item Clicked ${++clicked}`
+                                    it.onClick = () => {
+                                        log(`Click item at ${idx}`)
+                                        it.height += 10
+                                        it.nativeChannel(context, "getWidth")().then(
+                                            resolve => {
+                                                log(`resolve,${resolve}`)
+                                            },
+                                            reject => {
+                                                log(`reject,${reject}`)
+                                            })
+                                    }
+                                })
                             }
                         })
-                    ).also(it => {
-                        it.layoutConfig = {
-                            widthSpec: LayoutSpec.AT_MOST,
-                            heightSpec: LayoutSpec.WRAP_CONTENT,
-                        }
-                        it.onClick = () => {
-                            log(`Click item at ${idx}`)
-                            it.height += 10
-                            it.nativeChannel(context, "getWidth")().then(
-                                resolve => {
-                                    log(`resolve,${resolve}`)
-                                },
-                                reject => {
-                                    log(`reject,${reject}`)
-                                })
-                        }
                     })
                 },
-                layoutConfig: {
-                    widthSpec: LayoutSpec.AT_MOST,
-                    heightSpec: LayoutSpec.AT_MOST,
-                },
+                header: rotatedArrow(context),
+                content: list({
+                    itemCount: 0,
+                    renderItem: () => new ListItem,
+                    layoutConfig: {
+                        widthSpec: LayoutSpec.AT_MOST,
+                        heightSpec: LayoutSpec.AT_MOST,
+                    },
+                }),
             }),
+
         ]).also(it => {
             it.layoutConfig = {
                 widthSpec: LayoutSpec.AT_MOST,

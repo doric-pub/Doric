15d3a1cbf7eabc2fd45bd46a31873cd8d0cc7e30
王劲鹏 <jingpeng.wang@outlook.com>
2019-12-20 11:20:38 +0800

    add custom callback for async result

--- doric/template/custom_callback.h
+++ doric/template/custom_callback.h
@@ -1,6 +1,8 @@
 #ifndef CUSTOM_CALLBACK_H
 #define CUSTOM_CALLBACK_H
 
+#include <QDebug>
+
 #include "async/callback.h"
 
 template <class R>
@@ -8,11 +10,11 @@ class CustomCallback : public Callback<R> {
 
 public:
     void onResult(R result) override {
-
+        qDebug() << result;
     }
 
     void onError(QException exception) override {
-
+        qDebug() << exception.what();
     }
 
     void onFinish() override {

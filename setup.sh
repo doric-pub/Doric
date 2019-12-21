6c3060d35847ae9971464dd67c747767df137c2d
pengfei.zhou <pengfeizhou@foxmail.com>
2019-12-21 23:15:09 +0800

    update script

--- setup.sh
+++ setup.sh
@@ -4,8 +4,6 @@
 ##  Init Develop Environment
 ##
 ##############################################################################
-
-cd js-framework && npm install && npm run build && cd ..
-cd debugger && npm install && npm run build && cd ..
-cd demo && npm install && npm run build && cd ..
+cd doric-js && npm install && npm run build && cd ..
+cd doric-demo && npm install && npm run build && cd ..
 cd doric-cli && npm install && npm link && cd ..

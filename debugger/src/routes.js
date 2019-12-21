529ae56f7f6975edf20fb4e94ddf889163259b1a
pengfei.zhou <pengfeizhou@foxmail.com>
2019-12-21 23:12:32 +0800

    delete debugger

--- debugger/src/routes.js
+++
@@ -1,7 +0,0 @@
-import Home from './components/Home.vue'
-import Context from './components/Context.vue'
-
-export default [
-    { path: '/', component: Home },
-    { path: '/context/:id', component: Context }
-]

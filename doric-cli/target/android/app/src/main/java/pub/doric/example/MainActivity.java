c4777b5760bb4334de85669460e85a9104fcd14d
pengfei.zhou <pengfeizhou@foxmail.com>
2019-12-23 17:59:19 +0800

    feat:update cli target files

--- doric-cli/target/android/app/src/main/java/pub/doric/example/MainActivity.java
+++ doric-cli/target/android/app/src/main/java/pub/doric/example/MainActivity.java
@@ -9,25 +9,16 @@ import pub.doric.DoricFragment;
 
 public class MainActivity extends AppCompatActivity {
     private final String BUNDLE_NAME = "__$__";
-    private DoricFragment doricFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
-            String scheme = "assets://" + BUNDLE_NAME + ".js";
-            this.doricFragment = DoricFragment.newInstance(scheme, BUNDLE_NAME);
-            this.getSupportFragmentManager().beginTransaction().add(R.id.root, this.doricFragment).commit();
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (this.doricFragment.canPop()) {
-            this.doricFragment.pop();
-        } else {
-            super.onBackPressed();
+            String scheme = "assets://src/" + BUNDLE_NAME + ".js";
+            getIntent().putExtra("scheme", scheme);
+            getIntent().putExtra("alias", BUNDLE_NAME);
+            this.getSupportFragmentManager().beginTransaction().add(R.id.root, new DoricFragment()).commit();
         }
     }
 }

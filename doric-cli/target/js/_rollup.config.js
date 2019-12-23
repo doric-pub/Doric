4c0cb2b4129ec9d63ba9f4908dba350414493634
王劲鹏 <jingpeng.wang@outlook.com>
2019-12-23 14:38:34 +0800

    update template project

--- doric-cli/target/js/_rollup.config.js
+++ doric-cli/target/js/_rollup.config.js
@@ -1,5 +1,5 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import bundles from './build/index'
 import fs from 'fs'
 import path from 'path'

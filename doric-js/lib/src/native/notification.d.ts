9dabd3b1dbdf1b29af18b2f4d4f093b51f5399b5
pengfei.zhou <pengfeizhou@foxmail.com>
2020-01-09 09:54:08 +0800

    iOS:notification plugin

--- doric-js/lib/src/native/notification.d.ts
+++ doric-js/lib/src/native/notification.d.ts
@@ -1,13 +1,13 @@
 import { BridgeContext } from "../runtime/global";
 export declare function notification(context: BridgeContext): {
     publish: (args: {
-        biz: string;
+        biz?: string | undefined;
         name: string;
         data?: object | undefined;
         androidSystem?: boolean | undefined;
     }) => Promise<any>;
     subscribe: (args: {
-        biz: string;
+        biz?: string | undefined;
         name: string;
         callback: (data?: any) => void;
         androidSystem?: boolean | undefined;

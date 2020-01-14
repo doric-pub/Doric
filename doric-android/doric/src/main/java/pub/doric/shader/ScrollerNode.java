af64a47f7e46a3a163616159ddd8b4c498fc2fa5
王劲鹏 <jingpeng.wang@outlook.com>
2020-01-14 19:11:37 +0800

    remove old h&v scrollview

--- doric-android/doric/src/main/java/pub/doric/shader/ScrollerNode.java
+++ doric-android/doric/src/main/java/pub/doric/shader/ScrollerNode.java
@@ -21,7 +21,7 @@ import com.github.pengfeizhou.jscore.JSValue;
 
 import pub.doric.DoricContext;
 import pub.doric.extension.bridge.DoricPlugin;
-import pub.doric.widget.HVScrollView2;
+import pub.doric.widget.HVScrollView;
 
 /**
  * @Description: pub.doric.shader
@@ -29,7 +29,7 @@ import pub.doric.widget.HVScrollView2;
  * @CreateDate: 2019-11-18
  */
 @DoricPlugin(name = "Scroller")
-public class ScrollerNode extends SuperNode<HVScrollView2> {
+public class ScrollerNode extends SuperNode<HVScrollView> {
     private String mChildViewId;
     private ViewNode mChildNode;
 
@@ -50,12 +50,12 @@ public class ScrollerNode extends SuperNode<HVScrollView2> {
     }
 
     @Override
-    protected HVScrollView2 build() {
-        return new HVScrollView2(getContext());
+    protected HVScrollView build() {
+        return new HVScrollView(getContext());
     }
 
     @Override
-    protected void blend(HVScrollView2 view, String name, JSValue prop) {
+    protected void blend(HVScrollView view, String name, JSValue prop) {
         if ("content".equals(name)) {
             mChildViewId = prop.asString().value();
         } else {

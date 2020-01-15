87bd8ca0f9372533bedb84831d2c35ddee7dab7b
pengfei.zhou <pengfeizhou@foxmail.com>
2020-01-15 17:24:11 +0800

    js support es5

---
+++ doric-js/lib/index.runtime.es5.d.ts
@@ -0,0 +1,2 @@
+export * from 'core-js';
+export * from './src/runtime/sandbox';
--- doric-js/rollup.config.js
+++ doric-js/rollup.config.js
@@ -1,6 +1,6 @@
-const path = require('path')
-const fs = require('fs')
 import resolve from '@rollup/plugin-node-resolve'
+import buble from '@rollup/plugin-buble';
+import commonjs from '@rollup/plugin-commonjs'
 
 export default [
     {
@@ -19,6 +19,25 @@ export default [
         }
     },
     {
+        input: "lib-es5/index.runtime.es5.js",
+        output: {
+            name: "doric",
+            format: "iife",
+            file: "bundle/doric-sandbox.es5.js",
+        },
+        plugins: [
+            resolve({ mainFields: ["jsnext"] }),
+            commonjs(),
+            buble({
+                transforms: { dangerousForOf: true }
+            }),
+        ],
+        onwarn: function (warning) {
+            if (warning.code === 'THIS_IS_UNDEFINED') { return; }
+            console.warn(warning.message);
+        }
+    },
+    {
         input: "lib/index.js",
         output: {
             format: "cjs",
@@ -26,6 +45,24 @@ export default [
         },
         plugins: [
             resolve({ mainFields: ["jsnext"] }),
+        ],
+        external: ['reflect-metadata'],
+        onwarn: function (warning) {
+            if (warning.code === 'THIS_IS_UNDEFINED') { return; }
+            console.warn(warning.message);
+        }
+    },
+    {
+        input: "lib-es5/index.js",
+        output: {
+            format: "cjs",
+            file: "bundle/doric-lib.es5.js",
+        },
+        plugins: [
+            resolve({ mainFields: ["jsnext"] }),
+            buble({
+                transforms: { dangerousForOf: true }
+            }),
         ],
         external: ['reflect-metadata'],
         onwarn: function (warning) {

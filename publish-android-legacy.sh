2eddf9cc619b77c4b07486b7f75bc9786283e353
王劲鹏 <jingpeng.wang@outlook.com>
2020-01-16 16:05:10 +0800

    add android legacy publish shell script

---
+++ publish-android-legacy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+##############################################################################
+##
+##  Publish Android Legacy
+##
+##############################################################################
+
+CURRENT_DIR=$(cd $(dirname $0); pwd)
+CURRENT_VERSION=$(cat $CURRENT_DIR/version)
+
+echo "Current version is "$CURRENT_VERSION
+
+## Android
+echo "version="$CURRENT_VERSION > $CURRENT_DIR/doric-android/version.properties
+
+LATEST_HASH=$(git rev-parse origin/legacy/android_support)
+
+git cherry-pick $LATEST_HASH
+git tag "$CURRENT_VERSION"_legacy
+git push origin "$CURRENT_VERSION"_legacy
+
+echo "Publish Android"
+cd $CURRENT_DIR/doric-android && ./gradlew clean publishAll 

927615d6f16719dae79639b7087ea264e1c90f23
pengfei.zhou <pengfeizhou@foxmail.com>
2020-03-14 17:47:38 +0800

    update cli template

--- doric-cli/target/android/app/src/main/java/pub/doric/example/MainActivity.java
+++ doric-cli/target/android/app/src/main/java/pub/doric/example/MainActivity.java
@@ -1,24 +1,17 @@
 package pub.doric.example;
 
-
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import pub.doric.DoricFragment;
+import pub.doric.DoricActivity;
 
 public class MainActivity extends AppCompatActivity {
     private final String BUNDLE_NAME = "__$__";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String source = "assets://src/" + BUNDLE_NAME + ".js";
+        getIntent().putExtra("source", source);
+        getIntent().putExtra("alias", BUNDLE_NAME);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            String source = "assets://src/" + BUNDLE_NAME + ".js";
-            getIntent().putExtra("source", source);
-            getIntent().putExtra("alias", BUNDLE_NAME);
-            this.getSupportFragmentManager().beginTransaction().add(R.id.root, new DoricFragment()).commit();
-        }
     }
 }

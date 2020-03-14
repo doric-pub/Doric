b66df943a45cf17b1ec4b92d33be24ee819e049f
pengfei.zhou <pengfeizhou@foxmail.com>
2020-03-14 10:57:04 +0800

    Publish version 0.3.8

--- DoricCore.podspec
+++ DoricCore.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'DoricCore'
-  s.version          = '0.3.7'
+  s.version          = '0.3.8'
   s.summary          = 'Doric iOS SDK'
 
 
--- doric-js/lib/src/runtime/global.d.ts
+++ doric-js/lib/src/runtime/global.d.ts
@@ -37,7 +37,13 @@ declare global {
         screenWidth: number;
         screenHeight: number;
         statusBarHeight: number;
+        /**
+         * ex:Apple or Google
+         */
         deviceBrand: string;
+        /**
+         * ex: iPhone12,5 or pixel 3
+         */
         deviceModel: string;
         [index: string]: number | string | boolean | object | undefined;
     };

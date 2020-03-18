e3849e574aa5dc4036327694b0fbf5f054057498
王劲鹏 <jingpeng.wang@outlook.com>
2020-03-18 20:14:47 +0800

    android support compat

--- doric-android/doric/src/main/java/pub/doric/refresh/DoricRefreshView.java
+++ doric-android/doric/src/main/java/pub/doric/refresh/DoricRefreshView.java
@@ -1,16 +1,15 @@
 package pub.doric.refresh;
 
 import android.content.Context;
+import android.support.annotation.AttrRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.widget.FrameLayout;
-
-import androidx.annotation.AttrRes;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 /**
  * @Description: pub.doric.pullable

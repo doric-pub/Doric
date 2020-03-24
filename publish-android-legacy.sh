d22c3dfe12f671f3c3c4c69ff05f06690db5331c
pengfei.zhou <pengfeizhou@foxmail.com>
2020-03-24 19:37:30 +0800

    update publish script

--- publish-android-legacy.sh
+++ publish-android-legacy.sh
@@ -16,8 +16,6 @@ echo "version="$CURRENT_VERSION > $CURRENT_DIR/doric-android/version.properties
 LATEST_HASH=$(git rev-parse origin/legacy/android_support)
 
 git cherry-pick $LATEST_HASH
-git tag "$CURRENT_VERSION"_legacy
-git push origin "$CURRENT_VERSION"_legacy
 
 echo "Publish Android"
 cd $CURRENT_DIR/doric-android && ./gradlew clean publishAll

7939db48b596af9b544f15a39474ae0ceac57523
pengfei.zhou <pengfeizhou@foxmail.com>
2020-03-24 20:18:41 +0800

    update script

--- publish-android-legacy.sh
+++ publish-android-legacy.sh
@@ -17,5 +17,22 @@ LATEST_HASH=$(git rev-parse origin/legacy/android_support)
 
 git cherry-pick $LATEST_HASH
 
+git tag "$CURRENT_VERSION"_legacy
+
 echo "Publish Android"
 cd $CURRENT_DIR/doric-android && ./gradlew clean publishAll
+
+# get repo name from git
+remote=$(git config --get remote.origin.url)
+repo=$(basename $remote .git)
+
+commit=$(git rev-parse HEAD)
+
+curl -s -X POST https://api.github.com/repos/doric-pub/$repo/git/refs \
+-H "Authorization: token $GITHUB_TOKEN" \
+-d @- << EOF
+{
+  "ref": "refs/tags/'$CURRENT_VERSION'_legacy",
+  "sha": "$commit"
+}
+EOF

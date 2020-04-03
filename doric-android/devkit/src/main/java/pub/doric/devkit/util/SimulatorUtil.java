6ff085d579449656a8b28c868ee7bd26f355484b
王劲鹏 <jingpeng.wang@outlook.com>
2020-04-03 11:21:27 +0800

    change simulator check method

---
+++ doric-android/devkit/src/main/java/pub/doric/devkit/util/SimulatorUtil.java
@@ -0,0 +1,94 @@
+package pub.doric.devkit.util;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SimulatorUtil {
+    public static boolean isSimulator(Context context) {
+        return notHasBlueTooth()
+                || notHasLightSensorManager(context)
+                || isFeatures()
+                || checkIsNotRealPhone()
+                || checkPipes();
+    }
+
+    private static boolean notHasBlueTooth() {
+        BluetoothAdapter ba = BluetoothAdapter.getDefaultAdapter();
+        if (ba == null) {
+            return true;
+        } else {
+            String name = ba.getName();
+            return TextUtils.isEmpty(name);
+        }
+    }
+
+    private static boolean notHasLightSensorManager(Context context) {
+        SensorManager sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor8 = null;
+        if (sensorManager != null) {
+            sensor8 = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        }
+        return null == sensor8;
+    }
+
+    private static boolean isFeatures() {
+        return Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.toLowerCase().contains("vbox")
+                || Build.FINGERPRINT.toLowerCase().contains("test-keys")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86")
+                || Build.MANUFACTURER.contains("Genymotion")
+                || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+                || "google_sdk".equals(Build.PRODUCT);
+    }
+
+    private static boolean checkIsNotRealPhone() {
+        String cpuInfo = readCpuInfo();
+        return (cpuInfo.contains("intel") || cpuInfo.contains("amd"));
+    }
+
+    private static String readCpuInfo() {
+        String result = "";
+        try {
+            String[] args = {"/system/bin/cat", "/proc/cpuinfo"};
+            ProcessBuilder cmd = new ProcessBuilder(args);
+
+            Process process = cmd.start();
+            StringBuilder sb = new StringBuilder();
+            String readLine;
+            BufferedReader responseReader = new BufferedReader(new InputStreamReader(process.getInputStream(), "utf-8"));
+            while ((readLine = responseReader.readLine()) != null) {
+                sb.append(readLine);
+            }
+            responseReader.close();
+            result = sb.toString().toLowerCase();
+        } catch (IOException ignored) {
+        }
+        return result;
+    }
+
+    private static String[] known_pipes = {"/dev/socket/qemud", "/dev/qemu_pipe"};
+
+    private static boolean checkPipes() {
+        for (String pipes : known_pipes) {
+            File qemu_socket = new File(pipes);
+            if (qemu_socket.exists()) {
+                Log.v("Result:", "Find pipes!");
+                return true;
+            }
+        }
+        Log.i("Result:", "Not Find pipes!");
+        return false;
+    }
+}

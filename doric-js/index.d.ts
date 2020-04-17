e1825e4547f07103772d92a5f6252d6798ee4b69
王劲鹏 <jingpeng.wang@outlook.com>
2020-04-17 14:21:36 +0800

    update generated code

--- doric-js/index.d.ts
+++ doric-js/index.d.ts
@@ -486,12 +486,6 @@ declare module 'doric/lib/src/widget/image' {
                     width: number;
                     height: number;
             } | undefined) => void;
-            /**
-                * Stretch a image according to pointed rect
-                * Rect contains left, top, right & bottom
-                * For Android, it is based on nine patch
-                * For iOS, it is based on cap insets & stretch mode
-                */
             stretchInset?: {
                     left: number;
                     top: number;

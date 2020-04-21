22d0ae10fa6412468a84fc5c29ccdae9184168dd
王劲鹏 <jingpeng.wang@outlook.com>
2020-04-21 10:28:09 +0800

    update template projects

--- doric-cli/target/_rollup.config.js
+++ doric-cli/target/_rollup.config.js
@@ -3,8 +3,50 @@ import commonjs from '@rollup/plugin-commonjs'
 import bundles from './build/index'
 import fs from 'fs'
 import path from 'path'
-//import babel from 'rollup-plugin-babel';
-import json from '@rollup/plugin-json'
+import buble from '@rollup/plugin-buble'
+import json from "@rollup/plugin-json"
+import image from '@rollup/plugin-image'
+
+function searchImages(dir, images) {
+    const files = fs.readdirSync(dir)
+    files.forEach((item, index) => {
+        var fullPath = path.join(dir, item)
+        const stat = fs.statSync(fullPath)
+        if (stat.isDirectory()) {
+            searchImages(path.join(dir, item), images)
+        } else {
+            if(fullPath.endsWith('.png')) {
+                images.push(fullPath)
+            }
+        }
+    })
+    return images
+}
+
+const allImages = []
+searchImages('src', allImages)
+
+function mkdirsSync(dirname) {
+    if (fs.existsSync(dirname)) {
+        return true
+    } else {
+        if (mkdirsSync(path.dirname(dirname))) {
+            fs.mkdirSync(dirname)
+            return true
+        }
+    }
+}
+
+allImages.forEach((value) => {
+    let path = __dirname + '/build/' + value
+    let index = path.lastIndexOf('/')
+    mkdirsSync(path.substring(0, index))
+
+    fs.copyFile(__dirname + '/' + value, __dirname + '/build/' + value, error => {
+        console.log(error)
+    })
+})
+
 
 function readDirs(dirPath, files) {
     if (fs.statSync(dirPath).isDirectory()) {
@@ -51,15 +93,16 @@ export default
                     resolve({ mainFields: ["jsnext"] }),
                     commonjs(),
                     json(),
+                    image(),
                 ],
                 external: ['reflect-metadata', 'doric'],
                 onwarn: function (warning) {
-                    if (warning.code === 'THIS_IS_UNDEFINED') { return; }
-                    console.warn(warning.message);
+                    if (warning.code === 'THIS_IS_UNDEFINED') { return }
+                    console.warn(warning.message)
                 }
             }
         })
-// If need ES5 support enable following configs
+        // If need ES5 support enable following configs
         // .concat(
         //     allFiles
         //         .map(e => e.replace('.ts', ''))
@@ -74,14 +117,16 @@ export default
         //                 plugins: [
         //                     resolve({ mainFields: ["jsnext"] }),
         //                     commonjs(),
+        //                     json(),
         //                     buble({
         //                         transforms: { dangerousForOf: true }
         //                     }),
+        //                     image(),
         //                 ],
         //                 external: ['reflect-metadata', 'doric'],
         //                 onwarn: function (warning) {
-        //                     if (warning.code === 'THIS_IS_UNDEFINED') { return; }
-        //                     console.warn(warning.message);
+        //                     if (warning.code === 'THIS_IS_UNDEFINED') { return }
+        //                     console.warn(warning.message)
         //                 }
         //             }
         //         }))

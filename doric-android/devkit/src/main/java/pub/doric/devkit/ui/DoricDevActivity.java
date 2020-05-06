a539dfe1a6f95cc26bb8bacd16b00182ab67a542
王劲鹏 <jingpeng.wang@outlook.com>
2020-05-06 17:40:23 +0800

    android support compat

--- doric-android/devkit/src/main/java/pub/doric/devkit/ui/DoricDevActivity.java
+++ doric-android/devkit/src/main/java/pub/doric/devkit/ui/DoricDevActivity.java
@@ -4,18 +4,17 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import com.google.gson.JsonObject;
 import com.uuzuche.lib_zxing.activity.CaptureActivity;

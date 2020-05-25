9af44d0f0360679996c19b9de06d816f900cc090
pengfei.zhou <pengfeizhou@foxmail.com>
2020-05-25 10:21:30 +0000

    Release v0.4.26

--- doric-web/dist/index.js
+++ doric-web/dist/index.js
@@ -5231,75 +5231,78 @@ var doric_web = (function (exports, axios, sandbox) {
         }
     }
 
-    class PopoverPlugin extends DoricPlugin {
-        constructor(context) {
-            super(context);
-            this.fullScreen = document.createElement('div');
-            this.fullScreen.id = `PopOver__${context.contextId}`;
-            this.fullScreen.style.position = 'fixed';
-            this.fullScreen.style.top = '0px';
-            this.fullScreen.style.width = "100%";
-            this.fullScreen.style.height = "100%";
-        }
-        show(model) {
-            const viewNode = DoricViewNode.create(this.context, model.type);
-            if (viewNode === undefined) {
-                return Promise.reject(`Cannot create ViewNode for ${model.type}`);
+    let PopoverPlugin = /** @class */ (() => {
+        class PopoverPlugin extends DoricPlugin {
+            constructor(context) {
+                super(context);
+                this.fullScreen = document.createElement('div');
+                this.fullScreen.id = `PopOver__${context.contextId}`;
+                this.fullScreen.style.position = 'fixed';
+                this.fullScreen.style.top = '0px';
+                this.fullScreen.style.width = "100%";
+                this.fullScreen.style.height = "100%";
             }
-            viewNode.viewId = model.id;
-            viewNode.init();
-            viewNode.blend(model.props);
-            this.fullScreen.appendChild(viewNode.view);
-            let map = this.context.headNodes.get(PopoverPlugin.TYPE);
-            if (map) {
-                map.set(model.id, viewNode);
-            }
-            else {
-                map = new Map;
-                map.set(model.id, viewNode);
-                this.context.headNodes.set(PopoverPlugin.TYPE, map);
-            }
-            if (!document.body.contains(this.fullScreen)) {
-                document.body.appendChild(this.fullScreen);
-            }
-            return Promise.resolve();
-        }
-        dismiss(args) {
-            if (args) {
+            show(model) {
+                const viewNode = DoricViewNode.create(this.context, model.type);
+                if (viewNode === undefined) {
+                    return Promise.reject(`Cannot create ViewNode for ${model.type}`);
+                }
+                viewNode.viewId = model.id;
+                viewNode.init();
+                viewNode.blend(model.props);
+                this.fullScreen.appendChild(viewNode.view);
                 let map = this.context.headNodes.get(PopoverPlugin.TYPE);
                 if (map) {
-                    const viewNode = map.get(args.id);
-                    if (viewNode) {
-                        this.fullScreen.removeChild(viewNode.view);
-                    }
-                    if (map.size === 0) {
-                        document.body.removeChild(this.fullScreen);
+                    map.set(model.id, viewNode);
+                }
+                else {
+                    map = new Map;
+                    map.set(model.id, viewNode);
+                    this.context.headNodes.set(PopoverPlugin.TYPE, map);
+                }
+                if (!document.body.contains(this.fullScreen)) {
+                    document.body.appendChild(this.fullScreen);
+                }
+                return Promise.resolve();
+            }
+            dismiss(args) {
+                if (args) {
+                    let map = this.context.headNodes.get(PopoverPlugin.TYPE);
+                    if (map) {
+                        const viewNode = map.get(args.id);
+                        if (viewNode) {
+                            this.fullScreen.removeChild(viewNode.view);
+                        }
+                        if (map.size === 0) {
+                            document.body.removeChild(this.fullScreen);
+                        }
                     }
                 }
+                else {
+                    this.dismissAll();
+                }
+                return Promise.resolve();
             }
-            else {
+            dismissAll() {
+                let map = this.context.headNodes.get(PopoverPlugin.TYPE);
+                if (map) {
+                    for (let node of map.values()) {
+                        map.delete(node.viewId);
+                        this.fullScreen.removeChild(node.view);
+                    }
+                }
+                if (document.body.contains(this.fullScreen)) {
+                    document.body.removeChild(this.fullScreen);
+                }
+            }
+            onTearDown() {
+                super.onTearDown();
                 this.dismissAll();
             }
-            return Promise.resolve();
         }
-        dismissAll() {
-            let map = this.context.headNodes.get(PopoverPlugin.TYPE);
-            if (map) {
-                for (let node of map.values()) {
-                    map.delete(node.viewId);
-                    this.fullScreen.removeChild(node.view);
-                }
-            }
-            if (document.body.contains(this.fullScreen)) {
-                document.body.removeChild(this.fullScreen);
-            }
-        }
-        onTearDown() {
-            super.onTearDown();
-            this.dismissAll();
-        }
-    }
-    PopoverPlugin.TYPE = "popover";
+        PopoverPlugin.TYPE = "popover";
+        return PopoverPlugin;
+    })();
 
     class DoricListItemNode extends DoricStackNode {
     }

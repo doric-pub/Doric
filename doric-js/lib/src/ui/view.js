14f81783cacc1c8f3f66faa86f4499ba8498fb0b
刘涛 <aquietuncle@gmail.com>
2020-07-04 10:04:40 +0800

    feat:add view tag and findViewByTag

--- doric-js/lib/src/ui/view.js
+++ doric-js/lib/src/ui/view.js
@@ -54,6 +54,12 @@ export class View {
         }
         return f;
     }
+    findViewByTag(tag) {
+        if (tag === this.tag) {
+            return this;
+        }
+        return undefined;
+    }
     /** Anchor start*/
     get left() {
         return this.x;
@@ -289,6 +295,21 @@ export class Superview extends View {
                 return v;
             }
         }
+    }
+    findViewByTag(tag) {
+        if (tag === this.tag) {
+            return this;
+        }
+        return this.findViewTraversal(this, tag);
+    }
+    findViewTraversal(view, tag) {
+        for (let v of view.allSubviews()) {
+            let find = v.findViewByTag(tag);
+            if (find) {
+                return find;
+            }
+        }
+        return undefined;
     }
     isDirty() {
         if (super.isDirty()) {

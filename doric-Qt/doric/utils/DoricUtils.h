df8b98bc6cd59458ab1310a92e814a9dcb5c423c
王劲鹏 <jingpeng.wang@outlook.com>
2021-02-19 11:19:11 +0800

    change to set encoding

--- doric-Qt/doric/utils/DoricUtils.h
+++ doric-Qt/doric/utils/DoricUtils.h
@@ -13,7 +13,7 @@ public:
     QFile *file = new QFile(resource.fileName());
     file->open(QFile::ReadOnly | QFile::Text);
     QTextStream in(file);
-    in.setAutoDetectUnicode(true);
+    in.setEncoding(QStringConverter::Encoding::Utf8);
     QString content = in.readAll();
     file->close();
     delete file;

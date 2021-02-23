173cdb19502d9d84136c8517c1c98f2d8e4b57a8
pengfeizhou <pengfeizhou@foxmail.com>
2021-02-23 10:55:00 +0800

    feat:add assets dealing

--- doric-cli/src/actions.ts
+++ doric-cli/src/actions.ts
@@ -1,14 +1,27 @@
 import { Shell } from "./shell";
 import { createMergedSourceMapFromFiles } from "source-map-merger"
-import fs from "fs"
+import fs from "fs";
 import { glob } from "./util";
-
+import path from "path";
 export async function build() {
     await Shell.exec("node", ["node_modules/.bin/tsc", "-p", "."]);
     await Shell.exec("node", ["node_modules/.bin/rollup", "-c"]);
     const bundleFiles = await glob("bundle/**/*.js");
     for (let bundleFile of bundleFiles) {
         await doMerge(bundleFile);
+    }
+    if (fs.existsSync("assets")) {
+        const assets = await fs.promises.readdir("assets")
+        for (let asset of assets) {
+            const assetFile = path.resolve("assets", asset);
+            const stat = await fs.promises.stat(assetFile);
+            await Shell.exec("cp", ["-rf", assetFile, "bundle"]);
+            if (stat.isDirectory()) {
+                console.log(`Asset -> ${asset.yellow}`);
+            } else {
+                console.log(`Asset -> ${asset.green}`);
+            }
+        }
     }
 }
 

2e20515052425370333c452f2725f4987a7d4e93
pengfei.zhou <pengfeizhou@foxmail.com>
2021-03-02 15:11:16 +0800

    feat:add debug option:start debugging

--- doric-cli/src/shell.ts
+++ doric-cli/src/shell.ts
@@ -1,4 +1,4 @@
-import { spawn, SpawnOptionsWithoutStdio } from "child_process";
+import { ChildProcess, spawn, SpawnOptionsWithoutStdio } from "child_process";
 import "colors";
 const debug = false
 export namespace Shell {
@@ -59,6 +59,61 @@ export namespace Shell {
       });
     });
   }
+
+  export async function execProcess(
+    command: string,
+    args?: ReadonlyArray<string>,
+    options?: SpawnOptionsWithoutStdio & {
+      silence?: boolean; //不打印耗时,默认为false
+      verbose?: boolean; //打印输出内容,默认为true
+      consoleHandler?: (info: string) => void; //命令行输出内容处理
+    }
+  ): Promise<ChildProcess> {
+    if (!options?.silence) {
+      debug && console.log(`>>>>>>>>${command} ${args ? args.join(" ") : ""}`);
+    }
+    const now = new Date().getTime();
+    return new Promise((resolve) => {
+      const cmd = spawn(command, args, options);
+      cmd.stdout.on("data", (data) => {
+        if (options?.verbose !== false) {
+          debug && console.log(data.toString());
+        }
+        if (options?.consoleHandler) {
+          options.consoleHandler(data.toString());
+        }
+      });
+
+      cmd.stderr.on("data", (data) => {
+        if (options?.verbose !== false) {
+          debug && console.error(data.toString());
+        }
+        if (options?.consoleHandler) {
+          options.consoleHandler(data.toString());
+        }
+      });
+
+      cmd.on("close", (code) => {
+        if (!options?.silence) {
+          const cost = new Date().getTime() - now;
+          if (code !== 0) {
+            debug && console.log(`exitCode:${code} 耗时 ${cost}ms <<<<<<<<`.red);
+          } else {
+            debug && console.log(`exitCode:${code} 耗时 ${cost}ms <<<<<<<<`);
+          }
+        }
+      });
+      cmd.on("error", (err) => {
+        if (!options?.silence) {
+          const cost = new Date().getTime() - now;
+          debug && console.log(`error:${err} 耗时 ${cost}ms <<<<<<<<`);
+        }
+      });
+      resolve(cmd);
+    });
+  }
+
+
   export async function exec(
     command: string,
     args?: ReadonlyArray<string>,

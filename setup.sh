d13948223e886e58c0bd685cfa55900356e9cbda
pengfei.zhou <pengfeizhou@foxmail.com>
2021-03-02 18:27:06 +0800

    feat:update script

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@
 CURRENT_DIR=$(cd $(dirname $0); pwd)
 
 cd $CURRENT_DIR/doric-js && npm install && npm run build
+cd $CURRENT_DIR/doric-cli && npm install && npm link
 cd $CURRENT_DIR/doric-demo && npm install && npm run build
 cd $CURRENT_DIR/doric-web && npm install && npm run build
-cd $CURRENT_DIR/doric-cli && npm install && npm link

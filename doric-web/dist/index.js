d0db25d1c2d875977988a4157f6d4c5a5b2744da
pengfei.zhou <pengfeizhou@foxmail.com>
2021-03-08 06:51:31 +0000

    Release v0.7.6

--- doric-web/dist/index.js
+++ doric-web/dist/index.js
@@ -4420,6 +4420,7 @@ var doric_web = (function (exports, axios, sandbox) {
         }
     }
 
+    exports.LayoutSpec = void 0;
     (function (LayoutSpec) {
         LayoutSpec[LayoutSpec["EXACTLY"] = 0] = "EXACTLY";
         LayoutSpec[LayoutSpec["WRAP_CONTENT"] = 1] = "WRAP_CONTENT";
@@ -5900,6 +5901,8 @@ ${content}
     exports.registerViewNode = registerViewNode;
     exports.toPixelString = toPixelString;
     exports.toRGBAString = toRGBAString;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
 
     return exports;
 

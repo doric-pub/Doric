bf663e40dbd49bef77d6bb5bdcbdaad98aed2adb
王劲鹏 <jingpeng.wang@outlook.com>
2021-03-15 15:56:35 +0800

    simple demo layout config to just mode

--- doric-demo/src/SimpleDemo.ts
+++ doric-demo/src/SimpleDemo.ts
@@ -8,65 +8,78 @@ class LayoutDemo extends Panel {
                 stack([], {
                     width: 100,
                     height: 100,
-                    backgroundColor: Color.RED
+                    backgroundColor: Color.RED,
+                    layoutConfig: layoutConfig().just()
                 }),
                 stack([], {
                     width: 100,
                     height: 100,
-                    backgroundColor: Color.GREEN
+                    backgroundColor: Color.GREEN,
+                    layoutConfig: layoutConfig().just()
                 }),
                 stack([], {
                     width: 100,
                     height: 100,
-                    backgroundColor: Color.BLUE
+                    backgroundColor: Color.BLUE,
+                    layoutConfig: layoutConfig().just()
                 })
             ], {
                 width: 100,
                 height: 300,
+                layoutConfig: layoutConfig().just()
             }),
             vlayout([
                 stack([], {
                     width: 100,
                     height: 100,
-                    backgroundColor: Color.GREEN
+                    backgroundColor: Color.GREEN,
+                    layoutConfig: layoutConfig().just()
                 }),
                 stack([], {
                     width: 100,
                     height: 100,
-                    backgroundColor: Color.BLUE
+                    backgroundColor: Color.BLUE,
+                    layoutConfig: layoutConfig().just()
                 }),
                 stack([], {
                     width: 100,
                     height: 100,
-                    backgroundColor: Color.RED
+                    backgroundColor: Color.RED,
+                    layoutConfig: layoutConfig().just()
                 })
             ], {
                 width: 100,
                 height: 300,
+                layoutConfig: layoutConfig().just()
             }),
             vlayout([
                 stack([], {
                     width: 100,
                     height: 100,
-                    backgroundColor: Color.BLUE
+                    backgroundColor: Color.BLUE,
+                    layoutConfig: layoutConfig().just()
                 }),
                 stack([], {
                     width: 100,
                     height: 100,
-                    backgroundColor: Color.RED
+                    backgroundColor: Color.RED,
+                    layoutConfig: layoutConfig().just()
                 }),
                 stack([], {
                     width: 100,
                     height: 100,
-                    backgroundColor: Color.GREEN
+                    backgroundColor: Color.GREEN,
+                    layoutConfig: layoutConfig().just()
                 })
             ], {
                 width: 100,
                 height: 300,
+                layoutConfig: layoutConfig().just()
             })
         ], {
             width: 400,
             height: 300,
+            layoutConfig: layoutConfig().just(),
             backgroundColor: Color.BLACK
         }).in(rootView)
     }

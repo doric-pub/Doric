9fd6d0b2f570b98aca98bf7d57b639aa3db7dd8e
pengfei.zhou <pengfeizhou@foxmail.com>
2021-04-19 17:39:35 +0800

    iOS: fix when parent and parent's parent  is fit,child is most,cannot be extend to right size.

--- doric-iOS/Pod/Classes/Shader/DoricLayouts.m
+++ doric-iOS/Pod/Classes/Shader/DoricLayouts.m
@@ -194,10 +194,10 @@ static const void *kLayoutConfig = &kLayoutConfig;
         }
     }
     if (self.view.superview.doricLayout.layoutType != DoricUndefined && self.view.superview.doricLayout.widthSpec == DoricLayoutFit && self.widthSpec == DoricLayoutMost) {
-        self.measuredWidth = 0;
+        self.measuredWidth = targetSize.width;
     }
     if (self.view.superview.doricLayout.layoutType != DoricUndefined && self.view.superview.doricLayout.heightSpec == DoricLayoutFit && self.heightSpec == DoricLayoutMost) {
-        self.measuredHeight = 0;
+        self.measuredHeight = targetSize.height;
     }
 }
 

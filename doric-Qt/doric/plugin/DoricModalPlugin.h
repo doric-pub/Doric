14090b0d44da5c2a1b0229bdcb5207bea1c019af
王劲鹏 <jingpeng.wang@outlook.com>
2021-04-22 14:48:16 +0800

    add prompt

--- doric-Qt/doric/plugin/DoricModalPlugin.h
+++ doric-Qt/doric/plugin/DoricModalPlugin.h
@@ -14,9 +14,15 @@ public:
 
   Q_INVOKABLE void confirm(QString jsValueString, QString callbackId);
 
+  Q_INVOKABLE void prompt(QString jsValueString, QString callbackId);
+
   void onAccepted(QString callbackId);
 
+  void onAcceptedWithInput(QString callbackId, QString input);
+
   void onRejected(QString callbackId);
+
+  void onRejectedWithInput(QString callbackId, QString input);
 };
 
 #endif // DORICMODALPLUGIN_H
--- doric-Qt/doric/utils/DoricDialogBridge.cpp
+++ doric-Qt/doric/utils/DoricDialogBridge.cpp
@@ -34,3 +34,35 @@ void DoricDialogBridge::onRejected(QString windowPointer, QString pluginPointer,
     modalPlugin->onRejected(callbackId);
   }
 }
+
+void DoricDialogBridge::onAcceptedWithInput(QString windowPointer,
+                                            QString pluginPointer,
+                                            QString callbackId, QString input) {
+  {
+    QObject *object = (QObject *)(windowPointer.toULongLong());
+    QQuickWindow *window = dynamic_cast<QQuickWindow *>(object);
+    window->deleteLater();
+  }
+
+  {
+    QObject *object = (QObject *)(pluginPointer.toULongLong());
+    DoricModalPlugin *modalPlugin = dynamic_cast<DoricModalPlugin *>(object);
+    modalPlugin->onAcceptedWithInput(callbackId, input);
+  }
+}
+
+void DoricDialogBridge::onRejectedWithInput(QString windowPointer,
+                                            QString pluginPointer,
+                                            QString callbackId, QString input) {
+  {
+    QObject *object = (QObject *)(windowPointer.toULongLong());
+    QQuickWindow *window = dynamic_cast<QQuickWindow *>(object);
+    window->deleteLater();
+  }
+
+  {
+    QObject *object = (QObject *)(pluginPointer.toULongLong());
+    DoricModalPlugin *modalPlugin = dynamic_cast<DoricModalPlugin *>(object);
+    modalPlugin->onRejectedWithInput(callbackId, input);
+  }
+}

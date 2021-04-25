d1a00a716c7301bf78fb4aba94089a07d08a2f7a
王劲鹏 <jingpeng.wang@outlook.com>
2021-04-25 20:40:49 +0800

    fix network on pointed thread

--- doric-Qt/doric/plugin/DoricNetworkPlugin.h
+++ doric-Qt/doric/plugin/DoricNetworkPlugin.h
@@ -3,23 +3,11 @@
 
 #include "DoricNativePlugin.h"
 
-#include <QSettings>
-#include <QNetworkRequest>
-#include <QNetworkReply>
-#include <QNetworkAccessManager>
-
 class DoricNetworkPlugin : public DoricNativePlugin {
   Q_OBJECT
 public:
   using DoricNativePlugin::DoricNativePlugin;
 
   Q_INVOKABLE void request(QString jsValueString, QString callbackId);
-
-private:
-  QNetworkRequest httpRequest;
-  QNetworkAccessManager networkAccessManager;
-
-public slots:
-  void networkRequestFinished(QNetworkReply *reply);
 };
 #endif // DORICNETWORKPLUGIN_H

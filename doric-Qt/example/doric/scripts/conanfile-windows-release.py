d7dc8a6e11b1eea97785e2423b9f321562294d63
王劲鹏 <jingpeng.wang@outlook.com>
2021-05-13 14:16:07 +0800

    split debug & release for windows

---
+++ doric-Qt/example/doric/scripts/conanfile-windows-release.py
@@ -0,0 +1,14 @@
+from conans import ConanFile
+
+class DoricCore(ConanFile):
+    license = "MIT"
+    name = "DoricCore"
+    settings = "os", "compiler", "build_type", "arch"
+
+    def package(self):
+        self.copy("*.h", dst="include", src="../../doric")
+        self.copy("*.lib", dst="lib",src="../../../binary/doric/release")
+        self.copy("*", dst="qml",src="../resources")
+        self.copy("*.js", dst="js",src="../../../../doric-js/bundle")
+    def package_info(self):
+        self.cpp_info.libs = ["DoricCore"]

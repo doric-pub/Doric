dfdb60468cb95730e108300a532ab6d164db4d9a
pengfei.zhou <pengfeizhou@foxmail.com>
2021-07-13 17:24:48 +0800

    android:update snapshot control api

---
+++ doric-android/devkit/src/main/java/pub/doric/devkit/ui/DoricFloatingView.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright [2019] [Doric.Pub]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pub.doric.devkit.ui;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.MotionEvent;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+
+/**
+ * @Description: pub.doric.devkit.ui
+ * @Author: pengfei.zhou
+ * @CreateDate: 2021/7/13
+ */
+public class DoricFloatingView extends FrameLayout {
+    private int lastX;
+
+    private int lastY;
+
+    public DoricFloatingView(@NonNull Context context) {
+        super(context);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                lastX = x;
+                lastY = y;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int offsetX = x - lastX;
+                int offsetY = y - lastY;
+                FrameLayout.LayoutParams layoutParams = (LayoutParams) getLayoutParams();
+                layoutParams.leftMargin += offsetX;
+                layoutParams.topMargin += offsetY;
+                requestLayout();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+}

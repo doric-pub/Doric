5ff93efb5e7f85258ac158370daf7440440c9e6d
pengfei.zhou <pengfeizhou@foxmail.com>
2021-07-27 06:55:38 +0000

    Release v0.8.20

--- doric-js/bundle/doric-lib.es5.js
+++ doric-js/bundle/doric-lib.es5.js
@@ -3241,7 +3241,8 @@ function navigator(context) {
                 config.extra = JSON.stringify(config.extra);
             }
             return context.callNative(moduleName, 'push', {
-                source: source, config: config
+                source: source,
+                config: config
             });
         },
         pop: function (animated) {

eab2fa03bc241b39ce876bc52e5b9e4abecd7693
pengfei.zhou <pengfeizhou@foxmail.com>
2021-08-02 03:48:08 +0000

    Release v0.8.23

--- doric-web/dist/index.js
+++ doric-web/dist/index.js
@@ -3870,6 +3870,14 @@ __decorate$3([
 ], Input.prototype, "onSubmitEditing", void 0);
 __decorate$3([
     Property,
+    __metadata$3("design:type", Boolean)
+], Input.prototype, "enableHorizontalScrollBar", void 0);
+__decorate$3([
+    Property,
+    __metadata$3("design:type", Boolean)
+], Input.prototype, "enableVerticalScrollBar", void 0);
+__decorate$3([
+    Property,
     __metadata$3("design:type", Function)
 ], Input.prototype, "beforeTextChange", void 0);
 exports.InputType = void 0;
@@ -5204,7 +5212,7 @@ var doric_web = (function (exports, axios, sandbox) {
 	}());
 	});
 
-	var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+	var __awaiter$1 = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
 	    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
 	    return new (P || (P = Promise))(function (resolve, reject) {
 	        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -5216,7 +5224,7 @@ var doric_web = (function (exports, axios, sandbox) {
 	const loaders = [
 	    {
 	        filter: () => true,
-	        request: (source) => __awaiter(void 0, void 0, void 0, function* () {
+	        request: (source) => __awaiter$1(void 0, void 0, void 0, function* () {
 	            const result = yield axios__default['default'].get(source);
 	            return result.data;
 	        })
@@ -5226,7 +5234,7 @@ var doric_web = (function (exports, axios, sandbox) {
 	    loaders.push(loader);
 	}
 	function loadDoricJSBundle(source) {
-	    return __awaiter(this, void 0, void 0, function* () {
+	    return __awaiter$1(this, void 0, void 0, function* () {
 	        const matched = loaders.filter(e => e.filter(source));
 	        if (matched.length > 0) {
 	            return matched[matched.length - 1].request(source);
@@ -5243,6 +5251,7 @@ var doric_web = (function (exports, axios, sandbox) {
 	    }
 	}
 
+	exports.LayoutSpec = void 0;
 	(function (LayoutSpec) {
 	    LayoutSpec[LayoutSpec["EXACTLY"] = 0] = "EXACTLY";
 	    LayoutSpec[LayoutSpec["WRAP_CONTENT"] = 1] = "WRAP_CONTENT";
@@ -7648,7 +7657,7 @@ ${content}
 	    }
 	}
 
-	var __awaiter$1 = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+	var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
 	    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
 	    return new (P || (P = Promise))(function (resolve, reject) {
 	        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -7662,7 +7671,7 @@ ${content}
 	smoothscroll.polyfill();
 	registerDoricJSLoader({
 	    filter: (source) => source.startsWith("assets://"),
-	    request: (source) => __awaiter$1(void 0, void 0, void 0, function* () {
+	    request: (source) => __awaiter(void 0, void 0, void 0, function* () {
 	        const ret = yield axios__default['default'].get(source.replace("assets://", `${window.location.href}/../../doric-demo/bundle/`));
 	        return ret.data;
 	    })
@@ -7694,6 +7703,8 @@ ${content}
 	exports.registerViewNode = registerViewNode;
 	exports.toPixelString = toPixelString;
 	exports.toRGBAString = toRGBAString;
+
+	Object.defineProperty(exports, '__esModule', { value: true });
 
 	return exports;
 

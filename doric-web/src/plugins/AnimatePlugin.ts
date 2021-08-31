294ea66fa853b1fd0a7948c1c3a07dcc5529dbdd
pengfei.zhou <pengfeizhou@foxmail.com>
2021-08-31 18:40:03 +0800

    feat:fix compile error

--- doric-web/src/plugins/AnimatePlugin.ts
+++ doric-web/src/plugins/AnimatePlugin.ts
@@ -57,7 +57,6 @@ export class AnimatePlugin extends DoricPlugin {
                                 }
                             } catch (e) {
                                 console.error(e)
-                                alert(e.stack)
                             }
                         })
                     }) || [])

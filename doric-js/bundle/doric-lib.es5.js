71168decd964098fe270133f8164af2d7ec5a808
pengfei.zhou <pengfeizhou@foxmail.com>
2021-10-29 03:17:53 +0000

    Release v0.9.14

--- doric-js/bundle/doric-lib.es5.js
+++ doric-js/bundle/doric-lib.es5.js
@@ -1115,10 +1115,14 @@ var __read$2 = (undefined && undefined.__read) || function (o, n) {
     }
     return ar;
 };
-var __spreadArray$2 = (undefined && undefined.__spreadArray) || function (to, from) {
-    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
-        { to[j] = from[i]; }
-    return to;
+var __spreadArray$2 = (undefined && undefined.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) { for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) { ar = Array.prototype.slice.call(from, 0, i); }
+            ar[i] = from[i];
+        }
+    } }
+    return to.concat(ar || Array.prototype.slice.call(from));
 };
 function NativeCall(target, propertyKey, descriptor) {
     var originVal = descriptor.value;
@@ -1263,7 +1267,7 @@ var Panel = /** @class */ (function () {
         return this.renderSnapshots.length;
     };
     Panel.prototype.__restoreRenderSnapshot__ = function (idx) {
-        return __spreadArray$2([], __read$2(this.renderSnapshots)).slice(0, idx);
+        return __spreadArray$2([], __read$2(this.renderSnapshots), false).slice(0, idx);
     };
     Panel.prototype.__enableSnapshot__ = function () {
         this.snapshotEnabled = true;
@@ -2391,10 +2395,14 @@ var __read$1 = (undefined && undefined.__read) || function (o, n) {
     }
     return ar;
 };
-var __spreadArray$1 = (undefined && undefined.__spreadArray) || function (to, from) {
-    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
-        { to[j] = from[i]; }
-    return to;
+var __spreadArray$1 = (undefined && undefined.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) { for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) { ar = Array.prototype.slice.call(from, 0, i); }
+            ar[i] = from[i];
+        }
+    } }
+    return to.concat(ar || Array.prototype.slice.call(from));
 };
 var ListItem = /** @class */ (function (_super) {
     __extends$c(ListItem, _super);
@@ -2421,7 +2429,7 @@ var List = /** @class */ (function (_super) {
         return _this;
     }
     List.prototype.allSubviews = function () {
-        var ret = __spreadArray$1([], __read$1(this.cachedViews.values()));
+        var ret = __spreadArray$1([], __read$1(this.cachedViews.values()), false);
         if (this.loadMoreView) {
             ret.push(this.loadMoreView);
         }
@@ -3006,10 +3014,14 @@ var __read = (undefined && undefined.__read) || function (o, n) {
     }
     return ar;
 };
-var __spreadArray = (undefined && undefined.__spreadArray) || function (to, from) {
-    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
-        { to[j] = from[i]; }
-    return to;
+var __spreadArray = (undefined && undefined.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) { for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) { ar = Array.prototype.slice.call(from, 0, i); }
+            ar[i] = from[i];
+        }
+    } }
+    return to.concat(ar || Array.prototype.slice.call(from));
 };
 var FlowLayoutItem = /** @class */ (function (_super) {
     __extends$7(FlowLayoutItem, _super);
@@ -3037,7 +3049,7 @@ var FlowLayout = /** @class */ (function (_super) {
         return _this;
     }
     FlowLayout.prototype.allSubviews = function () {
-        var ret = __spreadArray([], __read(this.cachedViews.values()));
+        var ret = __spreadArray([], __read(this.cachedViews.values()), false);
         if (this.loadMoreView) {
             ret.push(this.loadMoreView);
         }
@@ -3671,7 +3683,8 @@ function navigator(context) {
                 config.extra = JSON.stringify(config.extra);
             }
             return context.callNative(moduleName, 'push', {
-                source: source, config: config
+                source: source,
+                config: config
             });
         },
         pop: function (animated) {

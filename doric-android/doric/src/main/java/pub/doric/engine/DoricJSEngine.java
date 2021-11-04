7014c7aa3280bf5688c53aef8f64a9e0479fbb1c
pengfei.zhou <pengfeizhou@foxmail.com>
2021-11-04 18:50:32 +0800

    Add webview init

--- doric-android/doric/src/main/java/pub/doric/engine/DoricJSEngine.java
+++ doric-android/doric/src/main/java/pub/doric/engine/DoricJSEngine.java
@@ -96,7 +96,7 @@ public class DoricJSEngine implements Handler.Callback, DoricTimerExtension.Time
     }
 
     protected void initJSEngine() {
-        mDoricJSE = new DoricNativeJSExecutor();
+        mDoricJSE = new DoricWebViewJSExecutor(Doric.application());
     }
 
     public void setEnvironmentValue(Map<String, Object> values) {
@@ -239,6 +239,9 @@ public class DoricJSEngine implements Handler.Callback, DoricTimerExtension.Time
 
     private void initDoricRuntime() {
         try {
+            if (mDoricJSE instanceof DoricWebViewJSExecutor) {
+                loadBuiltinJS("doric-web.js");
+            }
             loadBuiltinJS(DoricConstant.DORIC_BUNDLE_SANDBOX);
             String libName = DoricConstant.DORIC_MODULE_LIB;
             String libJS = DoricUtils.readAssetFile(DoricConstant.DORIC_BUNDLE_LIB);

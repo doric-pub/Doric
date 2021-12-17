5eb47355e0e77fc373197ef1a491937cb35e4a04
pengfei.zhou <pengfeizhou@foxmail.com>
2021-12-17 17:03:43 +0800

    iOS: add removeObserver in case addObserver being called multi times

--- doric-iOS/Pod/Classes/Shader/DoricImageNode.m
+++ doric-iOS/Pod/Classes/Shader/DoricImageNode.m
@@ -557,6 +557,13 @@
     } else if ([@"imageScale" isEqualToString:name]) {
         //Do not need set
     } else if ([@"onAnimationEnd" isEqualToString:name]) {
+        if (self.animationEndCallbackId) {
+#if DORIC_USE_YYWEBIMAGE
+            [(DoricImageView *) self.view removeObserver:self forKeyPath:@"currentIsPlayingAnimation" context:nil];
+#elif DORIC_USE_SDWEBIMAGE
+            [(DoricImageView *) self.view removeObserver:self forKeyPath:@"currentFrameIndex" context:nil];
+#endif
+        }
         self.animationEndCallbackId = prop;
         DoricImageView *doricImageView = (DoricImageView *) view;
 #if DORIC_USE_YYWEBIMAGE

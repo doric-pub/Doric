6362a0ddef9c6eb286bea667a4e9ca7029204cd2
吴尚昆 <shangkunwu@msn.com>
2022-03-18 17:05:32 +0800

    image tileInset property support boolean value.

--- doric-js/lib/src/widget/image.d.ts
+++ doric-js/lib/src/widget/image.d.ts
@@ -82,12 +82,19 @@ export declare class Image extends View {
         right: number;
         bottom: number;
     };
+    /**
+    * image tile
+    *
+    * If boolean value 'true' is passed, it equals { left: 0, top: 0, right: 0, bottom: 0 }
+    * Android: only support all area tile, so you'd better pass boolean value.
+    * iOS: support custom area tile, so you can pass object value and boolean value.
+    */
     tileInset?: {
         left: number;
         top: number;
         right: number;
         bottom: number;
-    };
+    } | boolean;
     /**
      * Called if loaded image is animated and played end.
      */

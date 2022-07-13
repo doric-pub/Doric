0887db69c7e32efcb734566cb907bb48c9ff588a
pengfei.zhou <pengfeizhou@foxmail.com>
2022-07-13 10:52:33 +0000

    Release v0.10.21

--- doric-js/lib/src/native/storage.d.ts
+++ doric-js/lib/src/native/storage.d.ts
@@ -1,7 +1,7 @@
 import { BridgeContext } from "../runtime/global";
 export declare function storage(context: BridgeContext): {
-    setItem: (key: string, value: string, zone?: string | undefined) => Promise<any>;
-    getItem: (key: string, zone?: string | undefined) => Promise<string>;
-    remove: (key: string, zone?: string | undefined) => Promise<any>;
+    setItem: (key: string, value: string, zone?: string) => Promise<any>;
+    getItem: (key: string, zone?: string) => Promise<string>;
+    remove: (key: string, zone?: string) => Promise<any>;
     clear: (zone: string) => Promise<any>;
 };

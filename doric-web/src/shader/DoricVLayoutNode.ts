e0bf09a3ca7f08e724589607ef6898ec5c05246e
wangxiangyuan <awxy115@163.com>
2023-09-11 11:40:02 +0800

    fix:web VLayout、HLayout margin

--- doric-web/src/shader/DoricVLayoutNode.ts
+++ doric-web/src/shader/DoricVLayoutNode.ts
@@ -43,9 +43,9 @@ export class DoricVLayoutNode extends DoricGroupViewNode {
                 e.view.style.flex = `${e.layoutConfig?.weight}`
             }
             e.view.style.marginTop = toPixelString(e.layoutConfig?.margin?.top || 0)
+            const bottomMargin = e.layoutConfig?.margin?.bottom || 0
             e.view.style.marginBottom = toPixelString(
-                (idx === this.childNodes.length - 1) ? 0 : this.space
-                    + (e.layoutConfig?.margin?.bottom || 0))
+                (idx === this.childNodes.length - 1) ? bottomMargin: this.space + bottomMargin)
             e.view.style.marginLeft = toPixelString(e.layoutConfig?.margin?.left || 0)
             e.view.style.marginRight = toPixelString(e.layoutConfig?.margin?.right || 0)
             if ((e.layoutConfig.alignment & LEFT) === LEFT) {

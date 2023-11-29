258f8c48a97965fcca6833f1674cc3b9e17570d7
pengfei.zhou <pengfeizhou@foxmail.com>
2023-11-29 14:48:18 +0800

    add doric-ohos module

---
+++ doric-ohos/doric-ohos/src/main/ets/components/lib/Container.ts
@@ -0,0 +1,103 @@
+import { DoricArkUI } from "./ext";
+
+declare const ViewStackProcessor: any;
+declare const Column: any;
+declare const Row: any;
+declare const Text: any;
+declare const Image: any;
+declare const Alignment: any;
+
+
+declare function loadDocument(instance: ViewPU): void;
+
+type UpdateFunc = (elmtId: number, isFirstRender: boolean) => void;
+
+declare abstract class ViewPU {
+  constructor(parent: ViewPU, localStorage: any, elmtId?: number)
+
+  markNeedUpdate(): void;
+  findChildById(compilerAssignedUniqueChildId: string): ViewPU;
+  syncInstanceId(): void;
+  isFirstRender(): boolean;
+  restoreInstanceId(): void;
+  finishUpdateFunc(elmtId: number): void;
+  getDeletedElemtIds(elmtIds: number[]): void; // caller allocates an empty Array<number>
+  deletedElmtIdsHaveBeenPurged(elmtIds: number[]): void; // caller provides filled Array<number>
+  isLazyItemRender(elmtId: number): boolean;
+  setCardId(cardId: number): void;
+  getCardId(): number;
+  resetRecycleCustomNode(): void;
+
+  markNeedUpdate(): void;
+  updateDirtyElements(): void;
+  protected abstract purgeVariableDependenciesOnElmtId(removedElmtId: number): void;
+  abstract initialRender(): void;
+  protected abstract rerender(): void;
+  protected abstract updateRecycleElmtId(oldElmtId: number, newElmtId: number): void;
+
+  abstract aboutToBeDeleted(): void;
+
+  public observeComponentCreation(compilerAssignedUpdateFunc: UpdateFunc): void
+}
+
+
+
+export function injectViewPU(view: ViewPU) {
+  console.log("view", typeof view, typeof DoricArkUI);
+  view.initialRender = () => {
+    view.observeComponentCreation((elmtId, isInitialRender) => {
+      ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+      Column.create();
+      Column.width('100%');
+      Column.height(`100%`);
+      Column.backgroundColor({ "id": 16777229, "type": 10001, params: [], "bundleName": "com.example.myapplication", "moduleName": "entry" });
+      if (!isInitialRender) {
+        Column.pop();
+      }
+      ViewStackProcessor.StopGetAccessRecording();
+    });
+    view.observeComponentCreation((elmtId, isInitialRender) => {
+      ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+      Text.create(`Counter 11111`);
+      Text.fontSize({ "id": 16777281, "type": 10002, params: [], "bundleName": "com.example.myapplication", "moduleName": "entry" });
+      Text.fontColor({ "id": 16777239, "type": 10001, params: [], "bundleName": "com.example.myapplication", "moduleName": "entry" });
+      Text.fontWeight(500);
+      Text.align(Alignment.Center);
+      if (!isInitialRender) {
+        Text.pop();
+      }
+      ViewStackProcessor.StopGetAccessRecording();
+    });
+    Text.pop();
+    view.observeComponentCreation((elmtId, isInitialRender) => {
+      ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+      Text.create('Click to count');
+      Text.fontSize("20fp");
+      Text.fontColor("#FFFFFF");
+      Text.fontWeight(500);
+      Text.align(Alignment.Center);
+      Text.margin({
+        top: 20
+      });
+      Text.padding({
+        left: 20, right: 20, top: 20, bottom: 20
+      });
+      Text.backgroundColor("#3498db");
+      Text.onClick(() => {
+        console.log("click1");
+        //hilog.info(0x0000, 'osborn', 'click');
+        //this.countData.counted++;
+        //console.log("click2")
+        //this.countData.counted++;
+        // @ts-ignore
+        view.markNeedUpdate()
+      });
+      if (!isInitialRender) {
+        Text.pop();
+      }
+      ViewStackProcessor.StopGetAccessRecording();
+    });
+    Text.pop();
+    Column.pop();
+  }
+}
---
+++ doric-ohos/doric-ohos/src/main/ets/components/lib/ext.ts
@@ -0,0 +1,45 @@
+import { Panel, Group, vlayout, layoutConfig, Gravity, text, Text, Color, navbar, AssetsResource, image } from "doric";
+
+export class DoricArkUI extends Panel {
+    onShow() {
+        navbar(context).setTitle("doric-arkui")
+    }
+    build(rootView: Group): void {
+        let number: Text
+        let count = 0
+        vlayout([
+            image({
+                image: new AssetsResource("logo_doric.png"),
+            }),
+            number = text({
+                textSize: 40,
+                text: '0',
+            }),
+            text({
+                text: "Click to count",
+                textSize: 20,
+                backgroundColor: Color.parse('#70a1ff'),
+                textColor: Color.WHITE,
+                onClick: () => {
+                    number.text = `${++count}`
+                },
+                layoutConfig: layoutConfig().just(),
+                width: 200,
+                height: 50,
+            }),
+        ])
+            .apply({
+                layoutConfig: layoutConfig().just().configAlignment(Gravity.Center),
+                width: 200,
+                height: 200,
+                space: 20,
+                border: {
+                    color: Color.BLUE,
+                    width: 1,
+                },
+                gravity: Gravity.Center,
+            })
+            .into(rootView)
+    }
+
+}

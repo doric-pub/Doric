1e4e7d6e15247db29d011b9e293acacb6e969dea
王劲鹏 <jingpeng.wang@outlook.com>
2023-12-05 18:00:54 +0800

    ohos: impl text max lines & max width & height

--- doric-ohos/doric-ohos/src/main/ets/components/node/TextNode.ts
+++ doric-ohos/doric-ohos/src/main/ets/components/node/TextNode.ts
@@ -1,50 +1,91 @@
-import { Color, Gravity, Text as DoricText } from 'doric';
-import { Alignment, getGlobalObject } from '../lib/sandbox';
-import { DoricViewNode } from '../lib/DoricViewNode';
+import { Color, Text as DoricText, Gravity } from 'doric'
+import { Alignment, getGlobalObject } from '../lib/sandbox'
+import { DoricViewNode } from '../lib/DoricViewNode'
 
-const Text = getGlobalObject("Text");
-
-const TextAlign = getGlobalObject("TextAlign");
+const Text = getGlobalObject("Text")
+const TextAlign = getGlobalObject("TextAlign")
+const FontStyle = getGlobalObject("FontStyle")
+const FontWeight = getGlobalObject("FontWeight")
 
 export class TextNode extends DoricViewNode<DoricText> {
-  TAG = Text;
+  TAG = Text
 
   pushing(v: DoricText) {
   }
 
   pop() {
-    Text.pop();
+    Text.pop()
   }
 
   blend(v: DoricText) {
-    Text.create(v.text??"");
-    const textAlignment = (v.textAlignment??Gravity.Center).toModel();
+    // text
+    Text.create(v.text??"")
+
+    // textAlignment
+    const textAlignment = (v.textAlignment??Gravity.Center).toModel()
     if ((textAlignment & Gravity.Top.val) === Gravity.Top.val) {
       //top
-      Text.align(Alignment.Top);
+      Text.align(Alignment.Top)
     } else if ((textAlignment & Gravity.Bottom.val) === Gravity.Bottom.val) {
       //bottom
-      Text.align(Alignment.Bottom);
+      Text.align(Alignment.Bottom)
     } else {
-      Text.align(Alignment.Center);
+      Text.align(Alignment.Center)
     }
     if ((textAlignment & Gravity.Left.val) === Gravity.Left.val) {
       //left
-      Text.textAlign(TextAlign.Start);
+      Text.textAlign(TextAlign.Start)
     } else if ((textAlignment & Gravity.Right.val) === Gravity.Right.val) {
       //right
-      Text.textAlign(TextAlign.End);
+      Text.textAlign(TextAlign.End)
     } else {
-      Text.textAlign(TextAlign.Center);
+      Text.textAlign(TextAlign.Center)
     }
 
+    // textSize
     if (v.textSize) {
-      Text.fontSize(v.textSize);
+      Text.fontSize(v.textSize)
     }
 
+    // textColor
     if (v.textColor instanceof Color) {
       Text.fontColor(v.textColor.toModel())
     }
+
+    // maxLines
+    if (v.maxLines) {
+      Text.maxLines(v.maxLines)
+    }
+
+    // fontStyle
+    if (v.fontStyle) {
+      switch (v.fontStyle) {
+        case "normal":
+          Text.fontStyle(FontStyle.Normal)
+          Text.fontWeight(FontWeight.Normal)
+          break
+        case "italic":
+          Text.fontStyle(FontStyle.Italic)
+          Text.fontWeight(FontWeight.Normal)
+          break
+        case "bold":
+          Text.fontStyle(FontStyle.Normal)
+          Text.fontWeight(FontWeight.Bold)
+          break
+        case "bold_italic":
+          Text.fontStyle(FontStyle.Italic)
+          Text.fontWeight(FontWeight.Bold)
+          break
+      }
+    }
+
+    // maxWidth & maxHeight
+    Text.constraintSize({
+      ...(v.maxWidth !== undefined ? { maxWidth: v.maxWidth } : {}),
+      ...(v.maxHeight !== undefined ? { maxHeight: v.maxHeight } : {}),
+    })
+
+    // commonConfig
     this.commonConfig(v)
   }
 }

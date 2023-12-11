c025b2b3c578e7b3ddcb86af66e96b2e310bf688
王劲鹏 <jingpeng.wang@outlook.com>
2023-12-11 17:21:18 +0800

    ohos: restore demo

--- doric-ohos/entry/src/main/ets/panel/ListDragDemo.ts
+++ doric-ohos/entry/src/main/ets/panel/ListDragDemo.ts
@@ -3,7 +3,7 @@ import { colors } from "./utils"
 
 export class ListDragDemo extends Panel {
 
-  private data = new Array(10000).fill(1)
+  private data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
   private list?: List
 

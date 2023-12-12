330cfebd2390a48e3e9be3806d474e7b4eadf981
王劲鹏 <jingpeng.wang@outlook.com>
2023-12-12 19:12:16 +0800

    ohos: add toast & alert & confirm

--- doric-ohos/doric-ohos/src/main/ets/components/plugin/ModalPlugin.ts
+++ doric-ohos/doric-ohos/src/main/ets/components/plugin/ModalPlugin.ts
@@ -1,12 +1,63 @@
 import Prompt from '@system.prompt'
 import { Gravity } from 'doric'
+import display from '@ohos.display'
 
 import { DoricPlugin } from '../lib/sandbox'
 
 export class ModalPlugin extends DoricPlugin {
   toast(props: any) {
-    Prompt.showToast({
-      message: props.msg,
+    return new Promise((resolve, reject) => {
+      const gravity = props.gravity
+
+      const displaySync = display.getDefaultDisplaySync()
+      if ((gravity & Gravity.Top.val) === Gravity.Top.val) {
+      } else if ((gravity & Gravity.Bottom.val) === Gravity.Bottom.val) {
+      } else if ((gravity & Gravity.CenterY.val) === Gravity.CenterY.val) {
+      }
+
+      Prompt.showToast({
+        message: props.msg,
+      })
+
+      resolve("")
+    })
+  }
+
+  alert(props: any) {
+    return new Promise((resolve, reject) => {
+      Prompt.showDialog({
+        message: props.msg,
+        title: props.title,
+        buttons: [{
+          text: props.okLabel,
+          color: "#666666"
+        }],
+        success: function (data) {
+          resolve(data)
+        },
+      })
+    })
+  }
+
+  confirm(props: any) {
+    return new Promise((resolve, reject) => {
+      Prompt.showDialog({
+        message: props.msg,
+        title: props.title,
+        buttons: [{
+          text: props.okLabel,
+          color: "#33dd44"
+        }, {
+          text: props.cancelLabel,
+          color: "#33dd44"
+        }],
+        success: function (data) {
+          resolve(data)
+        },
+        cancel: function (data: string, code: string) {
+          reject(data)
+        },
+      })
     })
   }
 }

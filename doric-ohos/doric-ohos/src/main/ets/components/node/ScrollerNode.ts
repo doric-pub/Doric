8c648c3353f95450ebf020dff18f672cd5304976
王劲鹏 <jingpeng.wang@outlook.com>
2023-12-13 09:17:57 +0800

    ohos: scroller add controller

--- doric-ohos/doric-ohos/src/main/ets/components/node/ScrollerNode.ts
+++ doric-ohos/doric-ohos/src/main/ets/components/node/ScrollerNode.ts
@@ -1,66 +1,68 @@
-import { Scroller } from 'doric';
-import { getGlobalObject, ViewStackProcessor } from '../lib/sandbox';
-import { DoricViewNode } from '../lib/DoricViewNode';
-import { createDoricViewNode } from '../lib/Registry';
+import { Scroller as DoricScroller } from 'doric'
+import { getGlobalObject, ViewStackProcessor } from '../lib/sandbox'
+import { createDoricViewNode } from '../lib/Registry'
+import { SuperNode } from '../lib/SuperNode'
 
-const ForEach = getGlobalObject("ForEach");
-const Scroll = getGlobalObject("Scroll");
-const Alignment = getGlobalObject("Alignment");
-const ScrollDirection = getGlobalObject("ScrollDirection");
-const BarState = getGlobalObject("BarState");
+const ForEach = getGlobalObject("ForEach")
+const Scroll = getGlobalObject("Scroll")
+const Alignment = getGlobalObject("Alignment")
+const ScrollDirection = getGlobalObject("ScrollDirection")
+const BarState = getGlobalObject("BarState")
+const Scroller = getGlobalObject("Scroller")
 
-export class ScrollerNode extends DoricViewNode<Scroller> {
-  TAG = Scroll;
+export class ScrollerNode extends SuperNode<DoricScroller> {
+  TAG = Scroll
 
-  childNodes: Map<string, DoricViewNode<any>> = new Map;
-  forEachElmtId?: number;
+  forEachElmtId?: number
 
-  pushing(v: Scroller) {
-    const firstRender = this.forEachElmtId === undefined;
+  private scroller = new Scroller()
+
+  pushing(v: DoricScroller) {
+    const firstRender = this.forEachElmtId === undefined
     if (!firstRender && !this.view.isDirty()) {
-      return;
+      return
     }
 
 
     if (firstRender) {
-      this.forEachElmtId = ViewStackProcessor.AllocateNewElmetIdForNextComponent();
+      this.forEachElmtId = ViewStackProcessor.AllocateNewElmetIdForNextComponent()
     }
     [v.content].forEach((child) => {
-      let childNode = this.childNodes.get(child.viewId);
+      let childNode = this.childNodes.get(child.viewId)
       if (childNode) {
-        childNode.render();
+        childNode.render()
       }
-    });
+    })
 
-    ViewStackProcessor.StartGetAccessRecordingFor(this.forEachElmtId);
-    ForEach.create();
-    const children = [v.content];
-    let diffIndexArray = []; // New indexes compared to old one.
-    let newIdArray = children.map(e => e.viewId);
-    let idDuplicates = [];
+    ViewStackProcessor.StartGetAccessRecordingFor(this.forEachElmtId)
+    ForEach.create()
+    const children = [v.content]
+    let diffIndexArray = [] // New indexes compared to old one.
+    let newIdArray = children.map(e => e.viewId)
+    let idDuplicates = []
 
-    ForEach.setIdArray(this.forEachElmtId, newIdArray, diffIndexArray, idDuplicates);
+    ForEach.setIdArray(this.forEachElmtId, newIdArray, diffIndexArray, idDuplicates)
 
     diffIndexArray.forEach((idx) => {
-      const child = children[idx];
-      let childNode = this.childNodes.get(child.viewId);
+      const child = children[idx]
+      let childNode = this.childNodes.get(child.viewId)
       if (!childNode) {
-        childNode = createDoricViewNode(this.context, child);
-        this.childNodes.set(child.viewId, childNode);
+        childNode = createDoricViewNode(this.context, child)
+        this.childNodes.set(child.viewId, childNode)
       }
-      ForEach.createNewChildStart(childNode.view.viewId, this.context.viewPU);
-      childNode.render();
-      ForEach.createNewChildFinish(childNode.view.viewId, this.context.viewPU);
-    });
+      ForEach.createNewChildStart(childNode.view.viewId, this.context.viewPU)
+      childNode.render()
+      ForEach.createNewChildFinish(childNode.view.viewId, this.context.viewPU)
+    })
 
     if (!firstRender) {
-      ForEach.pop();
+      ForEach.pop()
     }
-    ViewStackProcessor.StopGetAccessRecording();
+    ViewStackProcessor.StopGetAccessRecording()
     if (firstRender) {
-      ForEach.pop();
+      ForEach.pop()
     } else {
-      this.context.viewPU.finishUpdateFunc(this.forEachElmtId);
+      this.context.viewPU.finishUpdateFunc(this.forEachElmtId)
     }
   }
 
@@ -68,8 +70,8 @@ export class ScrollerNode extends DoricViewNode<Scroller> {
     Scroll.pop()
   }
 
-  blend(v: Scroller) {
-    Scroll.create()
+  blend(v: DoricScroller) {
+    Scroll.create(this.scroller)
     Scroll.scrollBar(BarState.Off)
     Scroll.align(Alignment.TopStart) // align to top start, otherwise default is center x
     Scroll.scrollable(ScrollDirection.Vertical)
